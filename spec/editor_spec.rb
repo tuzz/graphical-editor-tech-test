fbd01d46b5f79fadb347f335dd4ac363c31db3cd
Chris Patuzzo <chris@patuzzo.co.uk>
2015-07-31 13:35:46 +0100

    Write black-box tests for the examples in the readme

--- spec/editor_spec.rb
+++ spec/editor_spec.rb
@@ -1,5 +1,57 @@
 require "spec_helper"
 
-RSpec.describe Editor do
-  it "has some tests"
+RSpec.describe "Black-box test" do
+  it "produces the correct output for the first example in the readme" do
+    input = <<-INPUT
+I 5 6
+L 2 3 A
+S
+X
+    INPUT
+
+    expected = <<-OUTPUT
+> I 5 6
+> L 2 3 A
+> S
+
+=>
+OOOOO
+OOOOO
+OAOOO
+OOOOO
+OOOOO
+OOOOO
+    OUTPUT
+
+    actual = `echo '#{input}' | ./bin/editor`
+    expect(actual).to eq(expected)
+  end
+
+  it "produces the correct output for the second example in the readme" do
+    input = <<-INPUT
+F 3 3 J
+V 2 3 4 W
+H 3 4 2 Z
+S
+X
+    INPUT
+
+    expected = <<-OUTPUT
+> F 3 3 J
+> V 2 3 4 W
+> H 3 4 2 Z
+> S
+
+=>
+JJJJJ
+JJZZJ
+JWJJJ
+JWJJJ
+JJJJJ
+JJJJJ
+    OUTPUT
+
+    actual = `echo '#{input}' | ./bin/editor`
+    expect(actual).to eq(expected)
+  end
 end
